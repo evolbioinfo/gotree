aa4730ddadf8ecd1bc8d3f25b8775002e81b9c20
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-07-28 12:54:59 +0200

    Newick parser

--- lib/tree.go
+++ lib/tree.go
@@ -7,8 +7,10 @@ package lib
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
+	"regexp"
 	"strconv"
 )
 
@@ -124,13 +126,22 @@ func (t *Tree) Newick() string {
 	return t.root.Newick(nil) + ";"
 }
 
-func (n *Node) NeighborEdgeIndex(e *Edge) (int, error) {
+func (n *Node) EdgeIndex(e *Edge) (int, error) {
 	for i := 0; i < len(n.br); i++ {
 		if n.br[i] == e {
 			return i, nil
 		}
 	}
 	return -1, errors.New("The Edge is not in the neighbors of node")
+}
+
+func (n *Node) NodeIndex(next *Node) (int, error) {
+	for i := 0; i < len(n.neigh); i++ {
+		if n.neigh[i] == next {
+			return i, nil
+		}
+	}
+	return -1, errors.New("The Node is not in the neighbors of node")
 }
 
 // This function take the first node having 3 neighbors
@@ -190,12 +201,12 @@ func (t *Tree) GraftTipOnEdge(n *Node, e *Edge) error {
 	rnode := e.right
 
 	// index of edge in neighbors of l
-	e_l_ind, err := lnode.NeighborEdgeIndex(e)
+	e_l_ind, err := lnode.EdgeIndex(e)
 	if err != nil {
 		return err
 	}
 	// index of edge in neighbors of r
-	e_r_ind, err2 := rnode.NeighborEdgeIndex(e)
+	e_r_ind, err2 := rnode.EdgeIndex(e)
 	if err2 != nil {
 		return err2
 	}
@@ -277,6 +288,126 @@ func RandomBinaryTree(nbtips int) (*Tree, error) {
 	return t, err
 }
 
+func FromNewickFile(file string) (*Tree, error) {
+	dat, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	treeString := []rune(string(dat))
+	tree := NewTree()
+	_, err2 := FromNewickString(treeString, tree, nil, 0, 0)
+	if err2 != nil {
+		return nil, err2
+	}
+	return tree, nil
+}
+
+func FromNewickString(newick_str []rune, tree *Tree, curnode *Node, pos int, level int) (int, error) {
+	curchild := curnode
+	var e error
+
+	relen, erlen := regexp.Compile(`^\:(\d+(\.\d+){0,1}(e-\d+){0,1})`)
+	recomment, ercomment := regexp.Compile(`^\[([^,\(\)]*)\]`)
+	if erlen != nil {
+		return -1, erlen
+	}
+	if ercomment != nil {
+		return -1, ercomment
+	}
+
+	for pos < len(newick_str) {
+		var matchBrlen = relen.FindStringSubmatch(string(newick_str[pos:]))
+		var matchComment = recomment.FindStringSubmatch(string(newick_str[pos:]))
+		if pos == 0 && newick_str[0] != '(' {
+			return -1, errors.New("Newick file does not start with a \"(\" (Maybe not a Newick file?)")
+		}
+		if newick_str[pos] == '(' {
+			if level == 0 {
+				if curnode != nil {
+					return -1, errors.New("Malformed Newick: We should not be at recursion level 0 and having non nil node")
+				}
+				curnode = NewNode()
+				tree.SetRoot(curnode)
+				curnode.id = len(tree.nodes)
+				tree.nodes = append(tree.nodes, curnode)
+				pos, e = FromNewickString(newick_str, tree, curnode, pos+1, level+1)
+				if e != nil {
+					return -1, e
+				}
+				curchild = curnode
+			} else {
+				newnode := NewNode()
+				newedge := NewEdge()
+				newedge.id = len(tree.edges)
+				tree.edges = append(tree.edges, newedge)
+				newedge.SetLength(0.0)
+				newnode.id = len(tree.nodes)
+				tree.nodes = append(tree.nodes, newnode)
+				newedge.SetLeft(curnode)
+				newedge.SetRight(newnode)
+				curnode.AddChild(newnode, newedge)
+				newnode.AddChild(curnode, newedge)
+				curchild = newnode
+				pos, e = FromNewickString(newick_str, tree, curchild, pos+1, level+1)
+				if e != nil {
+					return -1, e
+				}
+			}
+		} else if newick_str[pos] == ')' {
+			pos++
+			if (level - 1) < 0 {
+				return -1, errors.New("Mismatched parentheses in Newick File (Maybe not a Newick file?)")
+			}
+			return pos, nil
+		} else if newick_str[pos] == ',' {
+			pos++
+		} else if len(matchComment) > 1 {
+			curnode.SetComment(matchComment[1])
+			pos += len([]rune(matchComment[0]))
+		} else if len(matchBrlen) > 1 {
+			// console.log(matchBrlen[0]);
+			nodeindex, err := curnode.NodeIndex(curchild)
+			if err != nil {
+				return -1, err
+			}
+			edge := curnode.br[nodeindex]
+			length, _ := strconv.ParseFloat(matchBrlen[1], 64)
+			edge.SetLength(length)
+			pos += len([]rune(matchBrlen[0]))
+		} else if newick_str[pos] == ';' {
+			if level != 0 {
+				return -1, errors.New("Mismatched parentheses in Newick File (Maybe not a Newick file?)")
+			}
+			pos++
+			return pos, nil
+		} else {
+			reg, e := regexp.Compile(`^([^(\[\]\(\)\:;\,)]*)`)
+			if e != nil {
+				return -1, e
+			}
+			match := reg.FindStringSubmatch(string(newick_str[pos:]))
+			// console.log(match[0]+" "+match[1]);
+			newnode := NewNode()
+			newedge := NewEdge()
+			newedge.id = len(tree.edges)
+			tree.edges = append(tree.edges, newedge)
+			newedge.SetLength(0.0)
+			newnode.id = len(tree.nodes)
+			tree.nodes = append(tree.nodes, newnode)
+
+			newedge.SetLeft(curnode)
+			newedge.SetRight(newnode)
+			curnode.AddChild(newnode, newedge)
+			newnode.AddChild(curnode, newedge)
+			curchild = newnode
+
+			curchild.SetName(match[1])
+			pos += len([]rune(match[0]))
+		}
+	}
+	return -1, errors.New("Reached end of file without a \";\"")
+}
+
 // Recursive function that outputs newick representation
 // from the current node
 func (n *Node) Newick(parent *Node) string {
@@ -285,18 +416,23 @@ func (n *Node) Newick(parent *Node) string {
 		if len(n.neigh) > 1 {
 			newick += "("
 		}
+		nbchild := 0
 		for i, child := range n.neigh {
 			if child != parent {
-				if i > 0 {
+				if nbchild > 0 {
 					newick += ","
 				}
 				newick += child.Newick(n)
+				if n.comment != "" {
+					newick += "[" + n.comment + "]"
+				}
 				if n.br[i].support != -1 {
 					newick += strconv.FormatFloat(n.br[i].support, 'f', 5, 64)
 				}
 				if n.br[i].length != -1 {
 					newick += ":" + strconv.FormatFloat(n.br[i].length, 'f', 5, 64)
 				}
+				nbchild++
 			}
 		}
 		if len(n.neigh) > 1 {
