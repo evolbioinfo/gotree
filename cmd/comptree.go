12d3da093bc05cd91e24e11cdbfdc0dae1b27c28
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-08-15 15:36:26 +0200

    Added Cobra CLI

--- cmd/comptree.go
+++ cmd/comptree.go
@@ -1,4 +1,18 @@
-package main
+// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
 
 import (
 	"bufio"
@@ -6,6 +20,7 @@ import (
 	"fmt"
 	"github.com/fredericlemoine/gotree/io/newick"
 	"github.com/fredericlemoine/gotree/tree"
+	"github.com/spf13/cobra"
 	"os"
 	"runtime"
 	"strconv"
@@ -104,12 +119,8 @@ func readCompTrees(inputfile string, compTrees chan<- trees) error {
 	return nil
 }
 
-func main() {
+func comptrees(tree1 string, tree2 string, tips bool, cpus int) {
 	maxcpus := runtime.NumCPU()
-	tree1 := flag.String("i", "stdin", "Input File containing the reference tree")
-	tree2 := flag.String("b", "none", "Input File containing trees to compare to reference tree (mandatory)")
-	tips := flag.Bool("tips", false, "If false, does not count tip edges")
-	cpus := flag.Int("t", 1, "Number of threads (Max=)"+strconv.Itoa(maxcpus))
 	var refTree *tree.Tree
 	var err error
 	var nbtips int
@@ -120,19 +131,19 @@ func main() {
 
 	flag.Parse()
 
-	if *tree2 == "none" {
+	if tree2 == "none" {
 		panic("You must provide a file containing compared trees")
 	}
-	if *cpus > maxcpus {
-		*cpus = maxcpus
+	if cpus > maxcpus {
+		cpus = maxcpus
 	}
 
-	fmt.Fprintf(os.Stderr, "Reference : %s\n", *tree1)
-	fmt.Fprintf(os.Stderr, "Compared  : %s\n", *tree2)
-	fmt.Fprintf(os.Stderr, "With tips : %b\n", *tips)
-	fmt.Fprintf(os.Stderr, "Threads   : %d\n", *cpus)
+	fmt.Fprintf(os.Stderr, "Reference : %s\n", tree1)
+	fmt.Fprintf(os.Stderr, "Compared  : %s\n", tree2)
+	fmt.Fprintf(os.Stderr, "With tips : %b\n", tips)
+	fmt.Fprintf(os.Stderr, "Threads   : %d\n", cpus)
 
-	if refTree, err = readRefTree(*tree1); err != nil {
+	if refTree, err = readRefTree(tree1); err != nil {
 		panic(err)
 	}
 	edges = refTree.Edges()
@@ -141,13 +152,13 @@ func main() {
 	}
 
 	go func() {
-		if err = readCompTrees(*tree2, compTreesChannel); err != nil {
+		if err = readCompTrees(tree2, compTreesChannel); err != nil {
 			panic(err)
 		}
 	}()
 
 	var wg sync.WaitGroup
-	for cpu := 0; cpu < *cpus; cpu++ {
+	for cpu := 0; cpu < cpus; cpu++ {
 		wg.Add(1)
 		go func(cpu int) {
 			for treeV := range compTreesChannel {
@@ -158,7 +169,7 @@ func main() {
 				if tree1, common, tree2, err = tree.CommonEdges(edges, edges2); err != nil {
 					panic(err)
 				}
-				if !*tips {
+				if !tips {
 					common -= nbtips
 				}
 				statsChannel <- stats{
@@ -181,4 +192,46 @@ func main() {
 	for stats := range statsChannel {
 		fmt.Printf("%d\t%d\t%d\t%d\n", stats.id, stats.tree1, stats.common, stats.tree2)
 	}
+}
+
+var comptreeCpus int
+var comptreeTree1 string
+var comptreeTree2 string
+var comptreeTips bool
+
+// comptreeCmd represents the comptree command
+var comptreeCmd = &cobra.Command{
+	Use:   "comptree",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		comptrees(comptreeTree1, comptreeTree2, comptreeTips, comptreeCpus)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(comptreeCmd)
+	maxcpus := runtime.NumCPU()
+
+	comptreeCmd.Flags().StringVarP(&comptreeTree1, "reftree", "i", "stdin", "Reference tree input file")
+	comptreeCmd.Flags().StringVarP(&comptreeTree2, "comptrees", "c", "none", "Compared trees input file")
+	comptreeCmd.Flags().BoolVarP(&comptreeTips, "tips", "l", false, "Compared trees input file")
+	comptreeCmd.Flags().IntVarP(&comptreeCpus, "threads", "t", 1, "Number of threads (Max=)"+strconv.Itoa(maxcpus))
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// comptreeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// comptreeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
 }
---
+++ cmd/gotree.go
@@ -0,0 +1,155 @@
+// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/fredericlemoine/gotree/io/newick"
+	"github.com/fredericlemoine/gotree/tree"
+	"github.com/spf13/cobra"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var gotreeSeed int64
+var gotreeNbTips int
+
+// gotreeCmd represents the gotree command
+var gotreeCmd = &cobra.Command{
+	Use:   "gotree",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		seed := gotreeSeed
+		nbtips := gotreeNbTips
+
+		fmt.Fprintf(os.Stderr, "Seed   : %d\n", seed)
+		fmt.Fprintf(os.Stderr, "Nb Tips: %d\n", nbtips)
+
+		rand.Seed(seed)
+
+		intree := "(t1:1,t2:2,(t3:3,(t4:4,t5:5)0.8:6)0.9:7);"
+		t, err2 := newick.NewParser(strings.NewReader(intree)).Parse()
+		if err2 != nil {
+			panic(err2)
+		}
+		fmt.Println(t.Newick())
+		if err3 := t.RemoveTips("t2", "t3"); err3 != nil {
+			panic(err3)
+		}
+
+		fmt.Println(t.Newick())
+
+		fmt.Println("Generating Tree")
+		t, err := tree.RandomBinaryTree(nbtips)
+		fmt.Println("Done")
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error()+"\n")
+		} else {
+			f, err := os.Create("/tmp/tree")
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("Writing to /tmp/tree")
+				f.WriteString(t.Newick() + "\n")
+				f.Close()
+				fmt.Println("Done")
+				// fmt.Println(t.Newick())
+				fmt.Println("Reading from /tmp/tree")
+				//gotree.FromNewickFile("/tmp/tree")
+				fmt.Println("Done")
+				fi, err := os.Open("t2.nh")
+				if err != nil {
+					panic(err)
+				}
+				defer func() {
+					if err := fi.Close(); err != nil {
+						panic(err)
+					}
+				}()
+				r := bufio.NewReader(fi)
+				tree, err := newick.NewParser(r).Parse()
+
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(tree.Newick())
+					for _, e := range tree.Edges() {
+						fmt.Println(e.DumpBitSet())
+					}
+					names := tree.AllTipNames()
+
+					for _, name := range names {
+						i, err := tree.TipIndex(name)
+						if err != nil {
+							fmt.Println(err)
+						} else {
+							fmt.Printf("name: %s | index: %d\n", name, i)
+						}
+					}
+					sort.Strings(names)
+					for _, name := range names {
+						i, err := tree.TipIndex(name)
+						if err != nil {
+							fmt.Println(err)
+						} else {
+							fmt.Println(name + " [" + strconv.Itoa(int(i)) + "]")
+						}
+					}
+					_, common, _, err := t.CommonEdges(tree, false)
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						fmt.Println("Common edges: " + strconv.Itoa(common))
+					}
+					_, _, common, err = tree.CommonEdges(tree, false)
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						fmt.Println("Common edges: " + strconv.Itoa(common))
+					}
+				}
+			}
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(gotreeCmd)
+	gotreeCmd.Flags().Int64VarP(&gotreeSeed, "seed", "s", time.Now().UTC().UnixNano(), "Seed (Optional)")
+	gotreeCmd.Flags().IntVarP(&gotreeNbTips, "nbtips", "t", 10, "Number of tips")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// gotreeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// gotreeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
