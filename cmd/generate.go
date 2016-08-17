9ba975d67fb8349c6188ec7290455a88e4b82483
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-08-17 10:51:03 +0200

    Removed default function for generate command

--- cmd/generate.go
+++ cmd/generate.go
@@ -15,8 +15,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -30,10 +28,10 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("generate called")
-	},
+	// Run: func(cmd *cobra.Command, args []string) {
+	// 	// TODO: Work your own magic here
+	// 	fmt.Println("generate called")
+	// },
 }
 
 func init() {
