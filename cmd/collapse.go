a23a8fefb298584845e7afffdc367f4873a619f0
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-08-18 17:46:44 +0200

    shorten collapse br length command

--- cmd/collapse.go
+++ cmd/collapse.go
@@ -15,12 +15,17 @@
 package cmd
 
 import (
+	"github.com/fredericlemoine/gotree/io/utils"
+	"github.com/fredericlemoine/gotree/tree"
 	"github.com/spf13/cobra"
+	"os"
 )
 
+var shortbranchesThreshold float64
+
 // collapseCmd represents the collapse command
-var collapseCmd = &cobra.Command{
-	Use:   "collapse",
+var collapsebrlenCmd = &cobra.Command{
+	Use:   "collapsebrlen",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -28,8 +33,37 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Read Tree
+		var t *tree.Tree
+		var err error
+		t, err = utils.ReadRefTree(transformInputTree)
+		if err != nil {
+			panic(err)
+		}
+		var f *os.File
+		if renameouttree != "stdout" {
+			f, err = os.Create(transformOutputTree)
+		} else {
+			f = os.Stdout
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		t.CollapseShortBranches(shortbranchesThreshold)
+
+		if err != nil {
+			panic(err)
+		}
+
+		f.WriteString(t.Newick() + "\n")
+		f.Close()
+	},
 }
 
 func init() {
-	transformCmd.AddCommand(collapseCmd)
+	transformCmd.AddCommand(collapsebrlenCmd)
+	collapsebrlenCmd.Flags().Float64VarP(&shortbranchesThreshold, "length", "l", 0.0, "Length cutoff to collapse the branch")
+
 }
