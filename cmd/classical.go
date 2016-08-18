c6b499e126a20030505fbab236b4a95159be2014
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-08-18 09:57:40 +0200

    refactor support classical

--- cmd/classical.go
+++ cmd/classical.go
@@ -15,96 +15,10 @@
 package cmd
 
 import (
-	"github.com/fredericlemoine/gotree/io/utils"
-	"github.com/fredericlemoine/gotree/tree"
+	"github.com/fredericlemoine/gotree/support"
 	"github.com/spf13/cobra"
 	"os"
-	"runtime"
-	"sync"
 )
-
-func classical(reftreefile, boottreefile string, outfile string, cpus int) {
-	var reftree *tree.Tree
-	var err error
-	var f *os.File
-	if outfile != "stdout" {
-		f, err = os.Create(outfile)
-	} else {
-		f = os.Stdout
-	}
-	if err != nil {
-		panic(err)
-	}
-
-	maxcpus := runtime.NumCPU()
-	if cpus > maxcpus {
-		cpus = maxcpus
-	}
-
-	if reftree, err = utils.ReadRefTree(reftreefile); err != nil {
-		panic(err)
-	}
-
-	if boottreefile == "none" {
-		panic("You must provide a file containing bootstrap trees")
-	}
-
-	if cpus > maxcpus {
-		cpus = maxcpus
-	}
-
-	var nbtrees int
-	compareChannel := make(chan utils.Trees, 100)
-	go func() {
-		if nbtrees, err = utils.ReadCompTrees(boottreefile, compareChannel); err != nil {
-			panic(err)
-		}
-	}()
-
-	edges := reftree.Edges()
-	foundEdges := make(chan int, 1000)
-	foundBoot := make([]int, len(edges))
-	var wg sync.WaitGroup
-	for cpu := 0; cpu < cpus; cpu++ {
-		wg.Add(1)
-		go func(cpu int) {
-			for treeV := range compareChannel {
-				edges2 := treeV.Tree.Edges()
-				for i, e := range edges {
-					if !e.Right().Tip() {
-						for _, e2 := range edges2 {
-							if !e2.Right().Tip() && e.SameBipartition(e2) {
-								foundEdges <- i
-								break
-							}
-						}
-					}
-				}
-
-			}
-			wg.Done()
-		}(cpu)
-	}
-
-	go func() {
-		wg.Wait()
-		close(foundEdges)
-	}()
-
-	for edge_i := range foundEdges {
-		foundBoot[edge_i]++
-	}
-
-	for i, count := range foundBoot {
-		if !edges[i].Right().Tip() {
-			edges[i].SetSupport(float64(count) / float64(nbtrees))
-		}
-	}
-
-	f.WriteString(reftree.Newick() + "\n")
-	f.Close()
-
-}
 
 var classicalEmpirical bool
 
@@ -119,7 +33,19 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		classical(supportIntree, supportBoottrees, supportOutFile, supportCpus)
+		var f *os.File
+		var err error
+		if supportOutFile != "stdout" {
+			f, err = os.Create(supportOutFile)
+		} else {
+			f = os.Stdout
+		}
+		if err != nil {
+			panic(err)
+		}
+		t := support.Classical(supportIntree, supportBoottrees, supportCpus)
+		f.WriteString(t.Newick() + "\n")
+		f.Close()
 	},
 }
 
---
+++ support/classical.go
@@ -0,0 +1,80 @@
+package support
+
+import (
+	"github.com/fredericlemoine/gotree/io/utils"
+	"github.com/fredericlemoine/gotree/tree"
+	"runtime"
+	"sync"
+)
+
+func Classical(reftreefile, boottreefile string, cpus int) *tree.Tree {
+	var reftree *tree.Tree
+	var err error
+
+	maxcpus := runtime.NumCPU()
+	if cpus > maxcpus {
+		cpus = maxcpus
+	}
+
+	if reftree, err = utils.ReadRefTree(reftreefile); err != nil {
+		panic(err)
+	}
+
+	if boottreefile == "none" {
+		panic("You must provide a file containing bootstrap trees")
+	}
+
+	if cpus > maxcpus {
+		cpus = maxcpus
+	}
+
+	var nbtrees int
+	compareChannel := make(chan utils.Trees, 100)
+	go func() {
+		if nbtrees, err = utils.ReadCompTrees(boottreefile, compareChannel); err != nil {
+			panic(err)
+		}
+	}()
+
+	edges := reftree.Edges()
+	foundEdges := make(chan int, 1000)
+	foundBoot := make([]int, len(edges))
+	var wg sync.WaitGroup
+	for cpu := 0; cpu < cpus; cpu++ {
+		wg.Add(1)
+		go func(cpu int) {
+			for treeV := range compareChannel {
+				edges2 := treeV.Tree.Edges()
+				for i, e := range edges {
+					if !e.Right().Tip() {
+						for _, e2 := range edges2 {
+							if !e2.Right().Tip() && e.SameBipartition(e2) {
+								foundEdges <- i
+								break
+							}
+						}
+					}
+				}
+
+			}
+			wg.Done()
+		}(cpu)
+	}
+
+	go func() {
+		wg.Wait()
+		close(foundEdges)
+	}()
+
+	for edge_i := range foundEdges {
+		foundBoot[edge_i]++
+	}
+
+	for i, count := range foundBoot {
+		if !edges[i].Right().Tip() {
+			edges[i].SetSupport(float64(count) / float64(nbtrees))
+		}
+	}
+
+	return reftree
+}
