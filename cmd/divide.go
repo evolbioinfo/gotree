888953937e97d1fb2e95756aeaefaa2744dabb2f
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-09-06 18:02:38 +0200

    Possible to divide an input tree file

---
+++ cmd/divide.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/fredericlemoine/gotree/io"
+	"github.com/fredericlemoine/gotree/io/utils"
+	"github.com/spf13/cobra"
+)
+
+var divideInputTree string
+var divideOutputTree string
+
+// divideCmd represents the divide command
+var divideCmd = &cobra.Command{
+	Use:   "divide",
+	Short: "Divides an input tree file into several tree files",
+	Long: `Divides an input tree file into several tree files
+
+If the input file contains several trees, lets say 10, then 10 output files 
+will be created, each containing 1 tree.
+
+Example:
+
+gotree divide -i trees.nw -o prefix_
+
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		var nbtrees int = 0
+		intrees := make(chan utils.Trees, 15)
+
+		/* Read ref tree(s) */
+		go func() {
+			if nbtrees, err = utils.ReadCompTrees(divideInputTree, intrees); err != nil {
+				io.ExitWithMessage(err)
+			}
+		}()
+
+		/* Dividing trees */
+		i := 0
+		for t := range intrees {
+			f := openWriteFile(fmt.Sprintf("%s_%03d.nw", divideOutputTree, i))
+			f.WriteString(t.Tree.Newick() + "\n")
+			f.Close()
+			i++
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(divideCmd)
+	divideCmd.PersistentFlags().StringVarP(&divideInputTree, "input", "i", "stdin", "Input tree(s) file")
+	divideCmd.PersistentFlags().StringVarP(&divideOutputTree, "output", "o", "prefix", "Divided trees output file prefix")
+}
