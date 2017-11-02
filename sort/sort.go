8f5bad92b99c4a3a023e1ef9ddd6c5b68c1c3d63
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2017-11-02 16:52:29 +0100

    Added sort functions

---
+++ sort/sort.go
@@ -0,0 +1,50 @@
+package sort
+
+import (
+	"sort"
+)
+
+// See https://stackoverflow.com/questions/42707252/
+
+type by struct {
+	Indices []int
+	Values  []int
+}
+
+func (b by) Len() int           { return len(b.Values) }
+func (b by) Less(i, j int) bool { return b.Values[i] < b.Values[j] }
+func (b by) Swap(i, j int) {
+	b.Indices[i], b.Indices[j] = b.Indices[j], b.Indices[i]
+	b.Values[i], b.Values[j] = b.Values[j], b.Values[i]
+}
+
+/*
+Sorts "toSort" array according to "byValues" Array.
+Only "toSort" array is modified.  "byValues" array is not modified.
+*/
+func SortIntBy(toSort []int, byValues []int) {
+	valuescopy := make([]int, len(byValues))
+	copy(valuescopy, byValues)
+	sort.Sort(by{Indices: toSort, Values: valuescopy})
+}
+
+/*
+Returns sorted indices by values
+Example:
+Input: [10,20,30,80,50]
+Outpt: [0,1,2,4,3]
+To range over the sorted array :
+for _,ord := range output {
+    input[ord]
+}
+*/
+func OrderInt(values []int) []int {
+	// init initial order indices
+	indices := make([]int, len(values))
+	for i, _ := range indices {
+		indices[i] = i
+	}
+	// Sort indices by values
+	SortIntBy(indices, values)
+	return indices
+}
---
+++ sort/sort_test.go
@@ -0,0 +1,110 @@
+package sort
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Tests the function to get neighboring edges of a given edges
+// Testing the edges of distance 1
+func TestSortIntBy1(t *testing.T) {
+	keys := []int{1, 2, 3, 4, 5}
+	values := []int{10, 20, 30, 80, 50}
+	expkeys := []int{1, 2, 3, 5, 4}
+	expvalues := []int{10, 20, 30, 80, 50}
+
+	SortIntBy(keys, values)
+
+	for i, v := range values {
+		if v != expvalues[i] {
+			t.Error(fmt.Sprintf("Values should not be reordered after SortIntBy function (%d: %d instead of %d)", i, v, expvalues[i]))
+		}
+	}
+
+	for i, v := range keys {
+		if v != expkeys[i] {
+			t.Error(fmt.Sprintf("Keys are not reordered appropriately after SortIntBy function (%d: %d instead of %d)", i, v, expkeys[i]))
+		}
+	}
+}
+
+func TestSortIntBy2(t *testing.T) {
+	keys := []int{5, 4, 3, 2, 1}
+	values := []int{10, 20, 30, 80, 50}
+	expkeys := []int{5, 4, 3, 1, 2}
+	expvalues := []int{10, 20, 30, 80, 50}
+
+	SortIntBy(keys, values)
+
+	for i, v := range values {
+		if v != expvalues[i] {
+			t.Error(fmt.Sprintf("Values should not be reordered after SortIntBy function (%d: %d instead of %d)", i, v, expvalues[i]))
+		}
+	}
+
+	for i, v := range keys {
+		if v != expkeys[i] {
+			t.Error(fmt.Sprintf("Keys are not reordered appropriately after SortIntBy function (%d: %d instead of %d)", i, v, expkeys[i]))
+		}
+	}
+}
+
+func TestOrderInt1(t *testing.T) {
+	values := []int{10, 20, 30, 80, 50}
+	expvalues := []int{10, 20, 30, 80, 50}
+	expIndices := []int{0, 1, 2, 4, 3}
+
+	indices := OrderInt(values)
+
+	for i, v := range values {
+		if v != expvalues[i] {
+			t.Error(fmt.Sprintf("Values should not be reordered after OrderInt function (%d: %d instead of %d)", i, v, expvalues[i]))
+		}
+	}
+
+	for i, v := range indices {
+		if v != expIndices[i] {
+			t.Error(fmt.Sprintf("Output Indices are not ordered appropriately after OrderInt function (%d: %d instead of %d)", i, v, expIndices[i]))
+		}
+	}
+}
+
+func TestOrderInt2(t *testing.T) {
+	values := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	expvalues := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	expIndices := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+
+	indices := OrderInt(values)
+
+	for i, v := range values {
+		if v != expvalues[i] {
+			t.Error(fmt.Sprintf("Values should not be reordered after OrderInt function (%d: %d instead of %d)", i, v, expvalues[i]))
+		}
+	}
+
+	for i, v := range indices {
+		if v != expIndices[i] {
+			t.Error(fmt.Sprintf("Output Indices are not ordered appropriately after OrderInt function (%d: %d instead of %d)", i, v, expIndices[i]))
+		}
+	}
+}
+
+func TestOrderInt3(t *testing.T) {
+	values := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expvalues := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expIndices := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	indices := OrderInt(values)
+
+	for i, v := range values {
+		if v != expvalues[i] {
+			t.Error(fmt.Sprintf("Values should not be reordered after OrderInt function (%d: %d instead of %d)", i, v, expvalues[i]))
+		}
+	}
+
+	for i, v := range indices {
+		if v != expIndices[i] {
+			t.Error(fmt.Sprintf("Output Indices are not ordered appropriately after OrderInt function (%d: %d instead of %d)", i, v, expIndices[i]))
+		}
+	}
+}
