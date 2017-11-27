c10a00a4db1e6ff73c56f2fd5a8bb16325580e8d
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2017-11-27 16:55:55 +0100

    Added function to scale branch supports

--- tree/tree.go
+++ tree/tree.go
@@ -1402,3 +1402,14 @@ func (t *Tree) DeepestNode() (maxnode *Node) {
 	}
 	return
 }
+
+// Scale branch supports by a given factor
+//
+// Does not do anything for branches with a support of NIL_SUPPORT
+func (t *Tree) ScaleSupports(factor float64) {
+	for _, e := range t.Edges() {
+		if s := e.Support(); s != NIL_SUPPORT {
+			e.SetSupport(s * factor)
+		}
+	}
+}
