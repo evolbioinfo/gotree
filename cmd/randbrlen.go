b458d23178d209817bad7749f54e936fe57d54c7
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-07-18 15:36:36 +0200

    Added min-mean and max-mean options to brlen setrand

--- cmd/randbrlen.go
+++ cmd/randbrlen.go
@@ -11,6 +11,8 @@ import (
 )
 
 var setlengthmean float64
+var setlengthmeanMin float64
+var setlengthmeanMax float64
 
 // randbrlenCmd represents the randbrlen command
 var randbrlenCmd = &cobra.Command{
@@ -18,8 +20,14 @@ var randbrlenCmd = &cobra.Command{
 	Short: "Assign a random length to edges of input trees",
 	Long: `Assign a random length to edges of input trees.
 
-Length follows an exponential distribution of parameter lambda=1/0.1 
-(Mean=0.1)
+Branch lengths are drawn in an exponential distribution of parameter lambda=1/mean.
+Two possibilities for the mean:
+
+1) If --mean-min and --mean-max are given, and mean-min < mean-max and are both > 0 then 
+"mean" is drawn uniformly in the interval [mean-min,mean-max]
+
+2) Otherwise, 'mean' is set to --mean value.
+
 `,
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		var f *os.File
@@ -38,14 +46,19 @@ Length follows an exponential distribution of parameter lambda=1/0.1
 		}
 		defer treefile.Close()
 
+		lmean := setlengthmean
 		for tr := range treechan {
+			if cmd.Flags().Changed("min-mean") && cmd.Flags().Changed("max-mean") &&
+				setlengthmeanMin < setlengthmeanMax && setlengthmeanMin >= 0 && setlengthmeanMax > 0 {
+				lmean = gostats.Float64RangeF(setlengthmeanMin, setlengthmeanMax)
+			}
 			if tr.Err != nil {
 				io.LogError(tr.Err)
 				return tr.Err
 			}
 
 			for _, e := range tr.Tree.Edges() {
-				e.SetLength(gostats.Exp(1.0 / setlengthmean))
+				e.SetLength(gostats.Exp(1.0 / lmean))
 			}
 			f.WriteString(tr.Tree.Newick() + "\n")
 		}
@@ -58,5 +71,7 @@ func init() {
 
 	randbrlenCmd.PersistentFlags().StringVarP(&intreefile, "input", "i", "stdin", "Input tree")
 	randbrlenCmd.Flags().Float64VarP(&setlengthmean, "mean", "m", 0.1, "Mean of the exponential distribution of branch lengths")
+	randbrlenCmd.Flags().Float64Var(&setlengthmeanMin, "min-mean", 0.001, "Mean of the exponential distribution of branch lengths will be drawn uniformly in the interval [min-mean,max-mean]")
+	randbrlenCmd.Flags().Float64Var(&setlengthmeanMax, "max-mean", 0.05, "Mean of the exponential distribution of branch lengths will be drawn uniformly in the interval [min-mean,max-mean]")
 	randbrlenCmd.PersistentFlags().StringVarP(&outtreefile, "output", "o", "stdout", "Random length output tree file")
 }
