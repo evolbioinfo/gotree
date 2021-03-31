f1023019bc845b2c2c68f3ae7e648fc37fcc83f2
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2021-03-31 14:13:50 +0200

    Corrected error.go

--- io/error.go
+++ io/error.go
@@ -14,20 +14,23 @@ const (
 
 func ExitWithMessage(err error) {
 	_, fn, line, _ := runtime.Caller(1)
-	name := strings.Split(fn, "/gotree/")[1]
+	cols := strings.Split(fn, "gotree/")
+	name := cols[len(cols)-1]
 	fmt.Fprintf(os.Stderr, "[Error] in %s (line %d), message: %v\n", name, line, err)
 	os.Exit(EXIT_FAILURE)
 }
 
 func LogError(err error) {
 	_, fn, line, _ := runtime.Caller(1)
-	name := strings.Split(fn, "/gotree/")[1]
+	cols := strings.Split(fn, "gotree/")
+	name := cols[len(cols)-1]
 	fmt.Fprintf(os.Stderr, "[Error] in %s (line %d), message: %v\n", name, line, err)
 }
 
 func LogWarning(err error) {
 	_, fn, line, _ := runtime.Caller(1)
-	name := strings.Split(fn, "/gotree/")[1]
+	cols := strings.Split(fn, "gotree/")
+	name := cols[len(cols)-1]
 	fmt.Fprintf(os.Stderr, "[Warning] in %s (line %d), message: %v\n", name, line, err)
 }
 
